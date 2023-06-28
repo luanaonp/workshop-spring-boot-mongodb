303a592294a1747496c525ada931295ec569cd92
Luana <luanaonp@gmail.com>
2023-06-28 11:06:08 -0300

    Obtendo um usuário por id

--- src/main/java/com/nelioalves/workshopmongo/domain/UserDTO.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/UserDTO.java
@@ -3,8 +3,7 @@ package com.nelioalves.workshopmongo.domain;
 import java.io.Serializable;
 
 public class UserDTO implements Serializable{
-	
-	
+		
 	private static final long serialVersionUID = 1L;
 	
 	private String id;
--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRepository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,14 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if(user ==null) {
+			 throw new ObjectNotFoundException("Objeto não encontrado");
+			}		
+		return user;
 	}
 	
 
