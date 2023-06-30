b5e5b27e4cf16210005bc6f48cab1aa688116047
Luana <luanaonp@gmail.com>
2023-06-29 21:50:25 -0300

    Deleção de usuário com delete

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -36,5 +36,10 @@ public class UserService {
 		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
 	}
 	
+	public void delete(String id) {
+		User user = findById(id);
+		repo.deleteById(id);
+	}
+	
 
 }
