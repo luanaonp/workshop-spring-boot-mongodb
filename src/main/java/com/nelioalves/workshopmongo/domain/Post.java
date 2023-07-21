93491e23c9013527f1518f17f111aada482bff4f
Luana <luanaonp@gmail.com>
2023-07-20 21:49:33 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,9 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+    private List<CommentDTO> list = new ArrayList<>();
+	
 	
 	public Post() {
 		
@@ -72,6 +78,15 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getList() {
+		return list;
+	}
+
+	public void setList(List<CommentDTO> list) {
+		this.list = list;
+	}
+
 
 	@Override
 	public int hashCode() {
